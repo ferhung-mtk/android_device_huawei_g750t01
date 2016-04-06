6f1fa863afc80f833aa84801b0b445ed3d7e6b34
ferhung-mtk <ferhung27@gmail.com>
2016-04-06 14:14:20 +0800

    MediaTek: Sepolicy hack for old kernel, our mt6582 & mt6592 version is 26.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -143,5 +143,8 @@ TARGET_CPU_MEMCPY_OPT_DISABLE := true
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
 
+# Sepolicy hack for old kernel, our mt6582 & mt6592 version is 26.
+POLICYVERS := 26
+
 # Hack for build
 $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
