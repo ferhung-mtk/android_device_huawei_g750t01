225ba39f8da58bf303eac47bd94003dbb0a56222
ferhung-mtk <ferhung27@gmail.com>
2016-05-18 17:03:02 +0800

    Twrp: set default language as zh_CN.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,6 +96,7 @@ TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
 TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
 TW_EXTRA_LANGUAGES := true
 TW_BUILD_ZH_CN_SUPPORT := true
+TW_DEFAULT_LANGUAGE := zh_CN
 TARGET_RECOVERY_INITRC := $(LOCAL_PATH)/recovery/init.rc
 
 # Bluetooth
